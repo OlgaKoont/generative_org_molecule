6ef398588e7e141ab3567848f70848a9622d7669
OlgaKoont <110968353+OlgaKoont@users.noreply.github.com>
2024-12-09 18:13:19 +0300

    Add files via upload

---
+++ freedpp/qed_sascore_freedpp/main_freedpp_7.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+python ../main_qed.py \
+	    --exp_root experiments \
+	        --alert_collections ../alert_collections.csv \
+		    --fragments ../zinc_crem.json \
+		        --receptor ../protein_A_6vn3.pdbqt \
+			   --vina_program ../env/qvina02 \
+			   --starting_smile "c1([*:1])c([*:2])ccc([*:3])c1" \
+			   --fragmentation crem \
+			   --num_sub_proc 12 \
+			   --n_conf 1 \
+                           --num_mols 1000 \
+			   --exhaustiveness 1 \
+			   --save_freq 50 \
+			   --epochs 200 \
+			   --commands "train,sample" \
+			   --reward_version soft \
+                           --box_center "28.111,0.895,29.799" \
+    		           --box_size "16.442,17.990,19.716" \
+			   --seed 7 \
+                           --weights "1.0" \
+                           --objectives "DockingScore" \
+                           --name freedpp_seed7 \
+                           #--checkpoint experiments/freedpp_seed7/ckpt/model_200.pth
+
+                           
+
